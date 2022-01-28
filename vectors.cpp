09481bdae0869f506b39e5b27681c941addfbc5f
Ekta Jain <71336762+ektaa1@users.noreply.github.com>
2022-01-28 23:50:34 +0530

    Add files via upload

---
+++ vectors.cpp
@@ -0,0 +1,21 @@
+#include<vector>
+#include<iostream>
+using namespace std;
+int main(){
+    vector<int> v;
+    cout<<"dcxc";
+    v.push_back(201);
+    v.push_back(89);
+    v.push_back(49);
+    v.push_back(492);
+    v.push_back(49);
+    v.push_back(49);
+    v.push_back(49);
+    v.push_back(49);
+    v.push_back(49);
+    v.pop_back();
+    cout<<v.capacity();
+    
+
+    
+}
